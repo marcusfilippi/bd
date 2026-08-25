bb1b1f132b902ec0043461fb022a7e52e4b6533f
Marcus Filippi <marcusfilippi5@gmail.com>
2026-08-25 17:28:09 -0300

    10.2  fazendo ainda

---
+++ lista4/exercicio10,2.sql
@@ -0,0 +1,19 @@
+update `cliente`
+set `telefone`='48998765432'
+where `cpf`='111.111.111-11';
+
+update `empregado`
+set `cargo`='Desenvolvedor Sênior'
+where `cpf`= '333.333.333-33';
+
+delete from `cliente`
+where `cpf`='555.555.555-55';
+
+-- 4
+
+
+-- 5
+
+delete from `projEmp`
+set 
+
