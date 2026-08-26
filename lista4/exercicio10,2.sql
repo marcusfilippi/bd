bfd83961a44f7ef73902a40e2885fdc14781ade4
Marcus <marcusfilippi5@gmail.com>
2026-08-26 08:59:39 -0300

    10.2 terminada

--- lista4/exercicio10,2.sql
+++ lista4/exercicio10,2.sql
@@ -23,7 +23,7 @@ where `cpf` = '555.555.555-55';
 
 -- 4
 update `projeto`
-set `preco`=`preco`*1.1
+set `preco`=`preco`*1.1;
 
 -- 5
 
@@ -38,5 +38,38 @@ where `dtSolicitacao`<'2025-09-15';
 
 -- 7
 
-update
+update `projEmp`
+set `hrTrab` = `hrTrab`+15
+where `cpfEmpregado` = '666.666.666-66' and `codProj`=3;
 
+-- 8
+
+delete from `projeto`
+where `preco` < 6000;
+
+-- 9
+
+update `projeto`
+set `dtEstimada` = DATE_ADD(`dtEstimada`, INTERVAL 7 DAY)
+where `cpfGerente` = '444.444.444-44';
+
+-- 10
+
+delete from `empregado`
+where `cpf`= '999.999.999-99';
+
+-- 11
+
+update `empregado`
+set `cargo`= 'Gerente de Projetos'
+where `cpf`= '333.333.333-33';
+
+update `projeto`
+set `cpfGerente` = '333.333.333-33'
+where `cpfGerente` = '222.222.222-22';
+
+-- 12
+
+delete from `projeto`
+where `dtFim`< current_date() 
+and `cpfCliente` <> '111.111.111-11';
